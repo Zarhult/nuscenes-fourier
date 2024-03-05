77fdcc9f37f25ca8761f1324b9589375d5ded9f7
Calvin Glisson <cglis001@ucr.edu>
2024-03-04 20:20:28 -0800

    Add random perturbation, phase visualization

--- main.py
+++ main.py
@@ -10,21 +10,38 @@ from PIL import Image
 
 # get the low frequency using Gaussian Low-Pass Filter
 # high d value tends to make high frequency info empty, low frequency info less blurred (intensifies high filter, weakens low filter)
-def gaussian_filter_low_pass(fshift, D):
+# in other words, high d value increases threshold for what counts as high frequency information rather than low
+def gaussian_filter_low_pass(fshift, D, perturb=False):
     h, w = fshift.shape
     x, y = np.mgrid[0:h, 0:w]
     center = (int((h - 1) / 2), int((w - 1) / 2))
     dis_square = (x - center[0]) ** 2 + (y - center[1]) ** 2
     template = np.exp(- dis_square / (2 * D ** 2)) # larger D value will make template closer to dis_square
+    if perturb:
+        real_noise_factor = np.random.normal(0.5, 0.5, dis_square.shape)
+        imaginary_noise_factor = np.random.normal(0.5, 0.5, dis_square.shape)
+        fshift.real = fshift.real * real_noise_factor
+        fshift.imag = fshift.imag * imaginary_noise_factor
+        #real_noise = np.random.normal(0, 99999, dis_square.shape)
+        #imaginary_noise = np.random.normal(0, 99999, dis_square.shape)
+        #fshift = fshift + real_noise + imaginary_noise * 1j
     return template * fshift
 
 # get the high frequency using Gaussian High-Pass Filter
-def gaussian_filter_high_pass(fshift, D):
+def gaussian_filter_high_pass(fshift, D, perturb=False):
     h, w = fshift.shape
     x, y = np.mgrid[0:h, 0:w]
     center = (int((h - 1) / 2), int((w - 1) / 2))
     dis_square = (x - center[0]) ** 2 + (y - center[1]) ** 2
     template = 1 - np.exp(- dis_square / (2 * D ** 2)) # larger D value will make template closer to 0
+    if perturb:
+        real_noise_factor = np.random.normal(0.5, 0.5, dis_square.shape)
+        imaginary_noise_factor = np.random.normal(0.5, 0.5, dis_square.shape)
+        fshift.real = fshift.real * real_noise_factor
+        fshift.imag = fshift.imag * imaginary_noise_factor
+        #real_noise = np.random.normal(0, 99999, dis_square.shape)
+        #imaginary_noise = np.random.normal(0, 99999, dis_square.shape)
+        #fshift = fshift + real_noise + imaginary_noise * 1j
     return template * fshift
 
 # Inverse Fourier transform
@@ -47,56 +64,71 @@ def f_shift_rgb(img):
     f_shift_rgb = np.array([f_shift_r, f_shift_g, f_shift_b])
     return f_shift_rgb
 
-def freq_domain(img):
-    '''Convert image to grayscale and return frequency domain representation of image.'''
+def magnitude_visualize(img):
+    '''Convert image to grayscale and return visualization of magnitudes of transformed image.'''
     image_grayscale = img.convert('L')
     img_arr = np.array(image_grayscale)
     img_arr_f_shift = f_shift(img_arr)
     # Calculate the magnitude spectrum, scaled so that it is useful for visualization
     magnitude_spectrum = 14*np.log(np.abs(img_arr_f_shift))
-    freq_image = Image.fromarray(magnitude_spectrum)
-    return freq_image
+    magnitude_image = Image.fromarray(magnitude_spectrum)
+    return magnitude_image
 
-def freq_domain_rgb(img):
+def phase_visualize(img):
+    '''Convert image to grayscale and return visualization of phase angles of transformed image.'''
+    image_grayscale = img.convert('L')
+    img_arr = np.array(image_grayscale)
+    img_arr_f_shift = f_shift(img_arr)
+    # Calculate the phase, scaled so that it is useful for visualization
+    phase = 40*np.angle(img_arr_f_shift)
+    phase_image = Image.fromarray(phase)
+    return phase_image
+
+def magnitude_visualize_rgb(img):
     img_f_shift_rgb = f_shift_rgb(img)
     h, w = img_f_shift_rgb[0].shape # doesn't matter which channel we use here
     rgbArray = np.zeros((h,w,3), 'uint8')
     rgbArray[:, :, 0] = 14*np.log(np.abs(img_f_shift_rgb[0]))
     rgbArray[:, :, 1] = 14*np.log(np.abs(img_f_shift_rgb[1]))
     rgbArray[:, :, 2] = 14*np.log(np.abs(img_f_shift_rgb[2]))
-    freq_image = Image.fromarray(rgbArray)
-    return freq_image
+    magnitude_image = Image.fromarray(rgbArray)
+    return magnitude_image
 
-def low_pass_rgb(img, D):
+def low_pass_rgb(img, D, perturb=False):
     img_f_shift_rgb = f_shift_rgb(img)
-    low_part_r = gaussian_filter_low_pass(img_f_shift_rgb[0].copy(), D=D)
-    low_part_g = gaussian_filter_low_pass(img_f_shift_rgb[1].copy(), D=D)
-    low_part_b = gaussian_filter_low_pass(img_f_shift_rgb[2].copy(), D=D)
+    low_part_r = gaussian_filter_low_pass(img_f_shift_rgb[0].copy(), D, perturb)
+    low_part_g = gaussian_filter_low_pass(img_f_shift_rgb[1].copy(), D, perturb)
+    low_part_b = gaussian_filter_low_pass(img_f_shift_rgb[2].copy(), D, perturb)
     low_rgb = np.array([low_part_r, low_part_g, low_part_b])
     return low_rgb
 
-def high_pass_rgb(img, D):
+def high_pass_rgb(img, D, perturb=False):
     img_f_shift_rgb = f_shift_rgb(img)
-    high_part_r = gaussian_filter_high_pass(img_f_shift_rgb[0].copy(), D=D)
-    high_part_g = gaussian_filter_high_pass(img_f_shift_rgb[1].copy(), D=D)
-    high_part_b = gaussian_filter_high_pass(img_f_shift_rgb[2].copy(), D=D)
+    high_part_r = gaussian_filter_high_pass(img_f_shift_rgb[0].copy(), D, perturb)
+    high_part_g = gaussian_filter_high_pass(img_f_shift_rgb[1].copy(), D, perturb)
+    high_part_b = gaussian_filter_high_pass(img_f_shift_rgb[2].copy(), D, perturb)
     high_rgb = np.array([high_part_r, high_part_g, high_part_b])
     return high_rgb
 
-def low_high_pass_rgb(img, D):
-    low_rgb = low_pass_rgb(img, D)
-    high_rgb = high_pass_rgb(img, D)
+def low_high_pass_rgb(img, D, perturb_low=False, perturb_high=False):
+    low_rgb = low_pass_rgb(img, D, perturb_low)
+    high_rgb = high_pass_rgb(img, D, perturb_high)
     low_high_rgb = np.array([low_rgb, high_rgb])
     return low_high_rgb
 
-def augment_image(r_img, i_img, D): # reference image, interference image
+def augment_image(r_img, i_img, D, augment_high=True): # reference image, interference image
     channel_num = random.randrange(0,3) # Randomize which channel to interfere with - 0 is red, 1 is green, 2 is blue
     i_img_low_high_parts = low_high_pass_rgb(i_img, D)
     r_img_low_high_parts = low_high_pass_rgb(r_img, D)
 
-    augmented_low_high_parts_r = r_img_low_high_parts[0][0] + (i_img_low_high_parts[1][0] if channel_num == 0 else r_img_low_high_parts[1][0])
-    augmented_low_high_parts_g = r_img_low_high_parts[0][1] + (i_img_low_high_parts[1][1] if channel_num == 1 else r_img_low_high_parts[1][1])
-    augmented_low_high_parts_b = r_img_low_high_parts[0][2] + (i_img_low_high_parts[1][2] if channel_num == 2 else r_img_low_high_parts[1][2])
+    if augment_high:
+        augmented_low_high_parts_r = r_img_low_high_parts[0][0] + (i_img_low_high_parts[1][0] if channel_num == 0 else r_img_low_high_parts[1][0])
+        augmented_low_high_parts_g = r_img_low_high_parts[0][1] + (i_img_low_high_parts[1][1] if channel_num == 1 else r_img_low_high_parts[1][1])
+        augmented_low_high_parts_b = r_img_low_high_parts[0][2] + (i_img_low_high_parts[1][2] if channel_num == 2 else r_img_low_high_parts[1][2])
+    else:
+        augmented_low_high_parts_r = r_img_low_high_parts[1][0] + (i_img_low_high_parts[0][0] if channel_num == 0 else r_img_low_high_parts[0][0])
+        augmented_low_high_parts_g = r_img_low_high_parts[1][1] + (i_img_low_high_parts[0][1] if channel_num == 1 else r_img_low_high_parts[0][1])
+        augmented_low_high_parts_b = r_img_low_high_parts[1][2] + (i_img_low_high_parts[0][2] if channel_num == 2 else r_img_low_high_parts[0][2])
     img_r = ifft(augmented_low_high_parts_r)
     img_g = ifft(augmented_low_high_parts_g)
     img_b = ifft(augmented_low_high_parts_b)
@@ -108,18 +140,16 @@ def augment_image(r_img, i_img, D): # reference image, interference image
     img = Image.fromarray(rgbArray)
     return img
 
-def augment_image_low(r_img, i_img, D): # reference image, interference image
-    channel_num = random.randrange(0,3) # Randomize which channel to interfere with - 0 is red, 1 is green, 2 is blue
-    i_img_low_high_parts = low_high_pass_rgb(i_img, D)
-    r_img_low_high_parts = low_high_pass_rgb(r_img, D)
-
-    augmented_low_high_parts_r = r_img_low_high_parts[1][0] + (i_img_low_high_parts[0][0] if channel_num == 0 else r_img_low_high_parts[0][0])
-    augmented_low_high_parts_g = r_img_low_high_parts[1][1] + (i_img_low_high_parts[0][1] if channel_num == 1 else r_img_low_high_parts[0][1])
-    augmented_low_high_parts_b = r_img_low_high_parts[1][2] + (i_img_low_high_parts[0][2] if channel_num == 2 else r_img_low_high_parts[0][2])
-    img_r = ifft(augmented_low_high_parts_r)
-    img_g = ifft(augmented_low_high_parts_g)
-    img_b = ifft(augmented_low_high_parts_b)
-    h, w = augmented_low_high_parts_r.shape # doesn't matter which channel we use here
+def perturb_image(img, D, perturb_high=True):
+    '''Perturb either high or low frequency information. Default high, otherwise low.'''
+    if perturb_high:
+        img_low_high_parts = low_high_pass_rgb(img, D, perturb_high=True)
+    else:
+        img_low_high_parts = low_high_pass_rgb(img, D, perturb_low=True)
+    img_r = ifft(img_low_high_parts[0][0]+img_low_high_parts[1][0])
+    img_g = ifft(img_low_high_parts[0][1]+img_low_high_parts[1][1])
+    img_b = ifft(img_low_high_parts[0][2]+img_low_high_parts[1][2])
+    h, w = img_low_high_parts[0][0].shape # doesn't matter which channel we use here
     rgbArray = np.zeros((h,w,3), 'uint8')
     rgbArray[:, :, 0] = img_r
     rgbArray[:, :, 1] = img_g
@@ -150,20 +180,31 @@ def main():
                         help='show high and low pass transforms on each channel of image')
     parser.add_argument('--augment-image', action='store_true', default=False,
                         help='show image augmented in the same manner as the FDA paper')
-    parser.add_argument('--freq-domain', action='store_true', default=False,
-                        help='show frequency domain representation of grayscaled image')
-    parser.add_argument('--freq-domain-rgb', action='store_true', default=False,
-                        help='show frequency domain representation of image (RGB)')
+    parser.add_argument('--magnitude-visualize', action='store_true', default=False,
+                        help='show visualization of magnitudes of transformed image')
+    parser.add_argument('--phase-visualize', action='store_true', default=False,
+                        help='show visualization of phase angles of transformed image')
+    parser.add_argument('--magnitude-visualize-rgb', action='store_true', default=False,
+                        help='show visualization of magnitudes of transformed image in RGB')
     parser.add_argument('--high-freq', action='store_true', default=False,
                         help='get the high frequency components of the reference image in RGB')
     parser.add_argument('--low-freq', action='store_true', default=False,
                         help='get the low frequency components of the reference image in RGB')
+    parser.add_argument('--perturb-low', action='store_true', default=False,
+                        help='show low-frequency perturbation of reference image in RGB')
+    parser.add_argument('--perturb-high', action='store_true', default=False,
+                        help='show high-frequency perturbation of reference image in RGB')
     parser.add_argument('--nuscenes-samples-preprocess', action='store_true', default=False,
                         help='save low and high frequency components of nuscenes samples to new directory "preproces"')
     parser.add_argument('--nuscenes-samples-augment-high', action='store_true', default=False,
                         help='save randomly high-frequency augmented versions of images')
     parser.add_argument('--nuscenes-samples-augment-low', action='store_true', default=False,
                         help='save randomly low-frequency augmented versions of images')
+    parser.add_argument('--nuscenes-samples-perturb-high', action='store_true', default=False,
+                        help='save randomly high-frequency perturbed versions of images')
+    parser.add_argument('--nuscenes-samples-perturb-low', action='store_true', default=False,
+                        help='save randomly low-frequency perturbed versions of images')
+    parser.add_argument('--test', action='store_true', default=False)
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     #use_mps = not args.no_mps and torch.backends.mps.is_available()
@@ -196,12 +237,15 @@ def main():
                 augmented_image.show()
             else:
                 print("Must provide interference image to augment the reference image")
-        elif args.freq_domain:
-            freq_image = freq_domain(image)
-            freq_image.show()
-        elif args.freq_domain_rgb:
-            freq_image_rgb = freq_domain_rgb(image)
-            freq_image_rgb.show()
+        elif args.magnitude_visualize:
+            magnitude_image = magnitude_visualize(image)
+            magnitude_image.show()
+        elif args.phase_visualize:
+            phase_image = phase_visualize(image)
+            phase_image.show()
+        elif args.magnitude_visualize_rgb:
+            magnitude_image = magnitude_visualize_rgb(image)
+            magnitude_image.show()
         elif args.high_freq:
             high_freq_img = high_pass_rgb(image, args.d)
             h,w = high_freq_img[0].shape
@@ -220,6 +264,42 @@ def main():
             rgbArray[:, :, 2] = ifft(low_freq_img[2])
             low_freq_img = Image.fromarray(rgbArray)
             low_freq_img.show()
+        elif args.perturb_low:
+            perturbed_image = perturb_image(image, args.d, perturb_high=False)
+            perturbed_image.show()
+        elif args.perturb_high:
+            perturbed_image = perturb_image(image, args.d, perturb_high=True)
+            perturbed_image.show()
+        elif args.test:
+            img = np.array(image)
+            f_shift_r = np.fft.fftshift(np.fft.fftn(img[:, :, 0]))
+            f_shift_g = np.fft.fftshift(np.fft.fftn(img[:, :, 1]))
+            f_shift_b = np.fft.fftshift(np.fft.fftn(img[:, :, 2]))
+            h,w = f_shift_r.shape
+            #real_noise = np.random.normal(0, 10000, (h,w))
+            #imaginary_noise = np.random.normal(0, 10000, (h,w))
+            #f_shift_r.real = f_shift_r.real + real_noise
+            #f_shift_r.imag = f_shift_r.imag + imaginary_noise
+            #f_shift_g.real = f_shift_g.real + real_noise
+            #f_shift_g.imag = f_shift_g.imag + imaginary_noise
+            #f_shift_b.real = f_shift_b.real + real_noise
+            #f_shift_b.imag = f_shift_b.imag + imaginary_noise
+            iimg_r = np.fft.ifftshift(f_shift_r)
+            iimg_r = np.fft.ifftn(iimg_r)
+            iimg_r = np.abs(iimg_r)
+            iimg_g = np.fft.ifftshift(f_shift_g)
+            iimg_g = np.fft.ifftn(iimg_g)
+            iimg_g = np.abs(iimg_g)
+            iimg_b = np.fft.ifftshift(f_shift_b)
+            iimg_b = np.fft.ifftn(iimg_b)
+            iimg_b = np.abs(iimg_b)
+            rgbArray = np.zeros((h,w,3), 'uint8')
+            rgbArray[:, :, 0] = iimg_r
+            rgbArray[:, :, 1] = iimg_g
+            rgbArray[:, :, 2] = iimg_b
+            img = Image.fromarray(rgbArray)
+            img.show()
+
     # todo: use refactored functions here
     elif args.nuscenes_samples_preprocess:
         samples_dir = '/share/data/nuscenes/samples'
@@ -322,11 +402,51 @@ def main():
                     new_filename_list.remove(image_filename) # so don't end up picking same image as interference image
                     interference_image_filename = random.choice(new_filename_list)
                     interference_image = Image.open(dir_full_path + '/' + interference_image_filename)
-                    augmented_image = augment_image_low(image, interference_image, args.d)
+                    augmented_image = augment_image(image, interference_image, args.d, augment_high=False)
                     image_filename_path = pathlib.Path(image_filename)
                     image_filename_no_ext = image_filename_path.with_suffix('') # get rid of extension
                     image_filename_ext = image_filename_path.suffix
                     augmented_image.save(save_dir + '/' + dir + '/' + str(image_filename_no_ext) + '_augmented' + image_filename_ext)
+    elif args.nuscenes_samples_perturb_low:
+        samples_dir = '/share/data/nuscenes/samples'
+        save_dir = '/share/data/nuscenes/perturbed_low'
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        for dir in os.listdir(samples_dir):
+            if dir[0:3] == 'CAM': # only process camera images
+                print(f'processing {dir}')
+                if not os.path.exists(save_dir + '/' + dir):
+                    os.makedirs(save_dir + '/' + dir)
+                dir_full_path = samples_dir + '/' + dir
+                for image_filename in os.listdir(dir_full_path):
+                    print(f'processing {image_filename}')
+                    image = Image.open(dir_full_path + '/' + image_filename)
+                    perturbed_image = perturb_image(image, args.d, perturb_high=False)
+                    image_filename_path = pathlib.Path(image_filename)
+                    image_filename_no_ext = image_filename_path.with_suffix('') # get rid of extension
+                    image_filename_ext = image_filename_path.suffix
+                    perturbed_image.save(save_dir + '/' + dir + '/' + str(image_filename_no_ext) + '_perturbed' + image_filename_ext)
+    elif args.nuscenes_samples_perturb_high:
+        samples_dir = '/share/data/nuscenes/samples'
+        save_dir = '/share/data/nuscenes/perturbed_high'
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        for dir in os.listdir(samples_dir):
+            if dir[0:3] == 'CAM': # only process camera images
+                print(f'processing {dir}')
+                if not os.path.exists(save_dir + '/' + dir):
+                    os.makedirs(save_dir + '/' + dir)
+                dir_full_path = samples_dir + '/' + dir
+                for image_filename in os.listdir(dir_full_path):
+                    print(f'processing {image_filename}')
+                    image = Image.open(dir_full_path + '/' + image_filename)
+                    perturbed_image = perturb_image(image, args.d, perturb_high=True)
+                    image_filename_path = pathlib.Path(image_filename)
+                    image_filename_no_ext = image_filename_path.with_suffix('') # get rid of extension
+                    image_filename_ext = image_filename_path.suffix
+                    perturbed_image.save(save_dir + '/' + dir + '/' + str(image_filename_no_ext) + '_perturbed' + image_filename_ext)
 
 if __name__ == '__main__':
     main()
